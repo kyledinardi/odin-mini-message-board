cfad4424b7b03755f13813d77293f47808abad88
kyledinardi <{ID}+{username}@users.noreply.github.com>
2024-05-08 14:20:52 -0400

    Write index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
 });
 
 module.exports = router;
